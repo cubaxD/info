a4d7c88889cd94f24ca8515fb3e7e13d2c27ed3d
cubaxD <ermando1398@gmail.com>
2026-07-02 22:50:30 -0300

    se agrega indicador de condicion actual

--- bmi.c
+++ bmi.c
@@ -11,6 +11,15 @@ int main(void) {
 	masa = peso / (altura * altura);
 	printf("Su indice de masa corporal es: %f ", masa);
 	
+	if(masa < 18.5){
+		printf("esta bajo de peso");
+	}else if(masa >= 18.5 && masa <=24.9){
+		printf("su peso es normal");
+	}else if(masa >=25 && masa <=29.9){
+		printf("tiene sobrepeso");
+	}else if(masa > 30){
+		printf("esta obeso");
+	}
 	printf("\nindice \t\t |\t condicion\n <18.5 \t\t | \t Bajo peso \n 18.5 a 24.9 \t | \t Normal \n 25.0 a 29.9 \t | \t Sobrepeso \n >=30 \t\t | \t Obesidad");
 	
 	
