35fe50cce16dd12833ce5bedc60f8a57acf6f661
cubaxD <ermando1398@gmail.com>
2026-07-10 16:56:26 -0300

    avance

--- bmi.c
+++ bmi.c
@@ -1,28 +1,40 @@
 #include <stdio.h>
 
 int main(void) {
-
-	float  peso, altura, masa;
+	float peso, altura, masa;
 	
-	printf("ingrese su peso en kg: ");
+	// Repositorio: https://github.com/tu-usuario/tu-repo   <-- reemplazá con el link real
+	
+	printf("Ingrese su peso en kg: ");
 	scanf("%f", &peso);
-	printf("ingrese su altura: ");
-	scanf("%f", &altura);
-	masa = peso / (altura * altura);
-	printf("Su indice de masa corporal es: %f ", masa);
-	
-	if(masa < 18.5){
-		printf("esta bajo de peso");
-	}else if(masa >= 18.5 && masa <=24.9){
-		printf("su peso es normal");
-	}else if(masa >=25 && masa <=29.9){
-		printf("tiene sobrepeso");
-	}else if(masa > 30){
-		printf("esta obeso");
+	while (peso <= 0) {
+		printf("Error: el peso no puede ser negativo ni cero.\n");
+		printf("Ingrese su peso en kg: ");
+		scanf("%f", &peso);
 	}
-	printf("\nindice \t\t |\t condicion\n <18.5 \t\t | \t Bajo peso \n 18.5 a 24.9 \t | \t Normal \n 25.0 a 29.9 \t | \t Sobrepeso \n >=30 \t\t | \t Obesidad");
 	
+	printf("Ingrese su altura en metros: ");
+	scanf("%f", &altura);
+	while (altura <= 0) {
+		printf("Error: la altura no puede ser negativa ni cero.\n");
+		printf("Ingrese su altura en metros: ");
+		scanf("%f", &altura);
+	}
+	
+	masa = peso / (altura * altura);
+	printf("Su indice de masa corporal es: %.2f\n", masa);
+	
+	if (masa < 18.5) {
+		printf("Esta bajo de peso");
+	} else if (masa >= 18.5 && masa <= 24.9) {
+		printf("Su peso es normal");
+	} else if (masa >= 25 && masa <= 29.9) {
+		printf("Tiene sobrepeso");
+	} else if (masa >= 30) {
+		printf("Esta obeso");
+	}
+	
+	printf("\nindice \t\t |\t condicion\n <18.5 \t\t | \t Bajo peso \n 18.5 a 24.9 \t | \t Normal \n 25.0 a 29.9 \t | \t Sobrepeso \n >=30 \t\t | \t Obesidad");
 	
 	return 0;
 }
-
